431d7a542acd835959416feb9a237272f652114b
AttackMAX <2646479700@qq.com>
2024-11-06 00:09:19 +0800

    起头式

---
+++ head.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define LL long long
+#define ld long double
+#define i64 __int128
+#define PII pair<int, int>
+#define endl '\n'
+#define pb push_back
+#define fi first
+#define se second
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+// cout<<setiosflags(ios::fixed)<<setprecision(K);
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+const double eps = 1e-9;
+const double PI = acos(-1.0);
+const int INF = 1e9 + 7;
+const int mod = 998244353;
+const int N = 2e5 + 10, M = N << 1;
+
+void solve()
+{
+}
+
+signed main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
